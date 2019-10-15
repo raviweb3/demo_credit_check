5bc7931a26692b2014e1dfca21c3254ef6592ff4
Appzoy <sai@appzoy.com>
2019-10-15 17:35:30 +0530

    designed table format for success message

--- index.js
+++ index.js
@@ -41,16 +41,27 @@ redirectRoute.route('/redirect').get(function(req, res) {
     let state = req.query.state;
 
     res.header('Content-Type', 'text/html');
-    res.write('<div style="background-color: #fff">'+
-        '<h6 style="color: #5fc35f; font-size: 2rem !important; text-align: center;padding: 1rem 0;">success</h6>'+
-        '<div style="padding: 1rem">'+
-        '<p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1.125rem !important; text-align: left">Name : RaviKiran</p>'+
-        '<p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1.125rem !important; text-align: left">Code:</p>'+
-        '<p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1.125rem !important; text-align: left">'+ code +'</p>'+
-        '<p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1.125rem !important; text-align: left">State:</p>'+
-        '<p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1.125rem !important; text-align: left">'+ state + '</p>'+
-        '</div>'+
-        '</div>')
+    res.write('<div id="root"><div style="background-color: #fff; width: 100%">'+
+        '<div style="background-color: #fff; width: 100%">'+
+            '<p style="color: #5fc35f; font-size: 2rem !important; text-align: center;padding: 1rem 0;">Success</p>'+
+        '</div>'+  
+        '<Table style="width: 100%; border: 1px solid #ddd">'+
+          '<thead>'+
+             '<tr>'+
+                '<th style="width:200px;color: #414658; font-size: 1rem !important">Name</th>'+
+                '<th color: #414658;font-size: 1rem !important">Code</th>'+
+                '<th style="width:200px;color: #414658;font-size: 1rem !important">State</th>'+
+            '</tr>'+
+            '</thead>'+
+            '<tbody>'+
+                '<tr>'+
+                    '<td><p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1rem !important; text-align: left">RaviKiran</p></td>'+
+                    '<td><p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1rem !important; text-align: left;word-break:break-all">'+ code +'</p></td>'+
+                    '<td><p style="color: #414658; margin-bottom: 0;padding: 0.5rem 0; font-size: 1rem !important; text-align: left">'+ state + '</p></td>'+
+                 '</tr>'+
+            +'</tbody>'+
+        '</Table>'+
+    +'</div></div>');
     res.end();
 });
 
